e2e9dbda7a9e73da33d69d23b82504df18aa2f54
Simon Bunker <simon@rendermania.com>
2022-06-09 23:14:25 +1000

    updated to tutorial

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
+import Paho from "paho-mqtt";
+
+import { useState, useEffect } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+
+client = new Paho.Client(
+  "broker.hivemq.com",
+  Number(8000),
+  `mqtt-async-test-${parseInt(Math.random() * 100)}`
+);
 
 export default function App() {
+
+  const [value, setValue] = useState(0);
+
+  function onMessage(message) {
+    if (message.destinationName === "mqtt-async-test/value")
+        setValue(parseInt(message.payloadString));
+  }
+
+  useEffect(() => {
+    client.connect( {
+      onSuccess: () => { 
+      console.log("Connected!");
+      client.subscribe("mqtt-async-test/value");
+      client.onMessageArrived = onMessage;
+    },
+    onFailure: () => {
+      console.log("Failed to connect!"); 
+    }
+  });
+  }, [])
+
+  function changeValue(c) {
+    const message = new Paho.Message((value + 1).toString());
+    message.destinationName = "mqtt-async-test/value";
+    c.send(message);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Value is: {value}</Text>
+      <Button onPress={() => { changeValue(client);} } title="Press Me"/>
       <StatusBar style="auto" />
     </View>
   );
